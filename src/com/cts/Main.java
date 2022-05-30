996abf7219e3d4ee13475393e5c6b321f8768098
ShikhaKumari28 <shikhatwinkle7@gmail.com>
2022-05-30 13:05:24 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,11 +1,16 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to session");
 		System.out.println("Hope u r able to follow");
+	
+		SomeService ss=new SomeService(); 
+		System.out.println(ss.findLength("program"));
 	}
 
 }
