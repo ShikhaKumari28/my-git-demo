242a6174072ff92100f19d1b5f067e6072a1c563
ShikhaKumari28 <shikhatwinkle7@gmail.com>
2022-05-30 10:58:46 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,11 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to session");
+		System.out.println("Hope u r able to follow");
+	}
+
+}
